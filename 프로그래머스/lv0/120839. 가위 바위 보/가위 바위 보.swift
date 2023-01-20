974c3467ef32e3dbd34770bbfcd798ff22ea11c2
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-20 13:09:50 +0900

    [level 0] Title: 가위 바위 보, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.swift
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.swift
@@ -1,22 +1,5 @@
 import Foundation
 
 func solution(_ rsp:String) -> String {
-    var answer = ""
-    for char in rsp {
-        answer += windCase(char)
-    }
-    return answer
-}
-
-func windCase(_ rsp: Character) -> String {
-    if rsp == "2" {
-        return "0"
-    }
-    if rsp == "0" {
-        return "5"
-    }
-    if rsp == "5" {
-        return "2"
-    }
-    return ""
+    return rsp.map( {$0 == "2" ? "0" : $0 == "0" ? "5" : "2"} ).joined()
 }
