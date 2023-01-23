3e6e27cb88144808228000bb9ccd12b6f2fe220d
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-23 20:40:11 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.swift
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ n:Int, _ numlist:[Int]) -> [Int] {
-    var answer: [Int] = []
-    for i in numlist {
-        if i % n == 0 {
-            answer.append(i)
-        }
-    }
-    return answer
+    return numlist.filter( { $0 % n == 0 } )
 }
