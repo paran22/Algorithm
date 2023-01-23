3678a4047680e4f3b93b8deeaf12c0b478f3c09f
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-23 20:59:10 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.swift
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    var reverseds : [Character] = []
-    for i in 0..<my_string.count {
-        reverseds.append(my_string[my_string.index(my_string.startIndex, offsetBy: i)])
-    }
-    return String(reverseds.reversed())
+    return my_string.reduce("", { String($1) + String($0) } )
 }
