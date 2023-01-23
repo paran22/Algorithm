5bedf60a97006bf7dace7defc475c8380d5672b5
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-23 20:47:02 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    if angle < 90 {
+        return 1
+    }
+    if angle == 90 {
+        return 2
+    }
+    if angle < 180 {
+        return 3
+    }
+    if angle == 180 {
+        return 4
+    }
+    return 0
+}
