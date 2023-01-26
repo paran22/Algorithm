f1867dbe422ddfc55f1a5ad74f2a7b38f4826572
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-26 20:44:46 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.swift
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.swift
@@ -2,8 +2,5 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     let num : Int = Int(sqrt(Double(n)))
-    if (num * num == n) {
-        return 1
-    }
-    return 2
+    return num * num == n ? 1 : 2
 }
