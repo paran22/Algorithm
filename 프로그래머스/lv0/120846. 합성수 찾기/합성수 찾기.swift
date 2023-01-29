a08e0aef0ab508e2623a4b1e4de8b5fa28831e2b
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-29 19:09:03 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.swift
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var answer = 0
-    for i in 1...n {
-        if (1...i).filter( { i % $0 == 0} ).count > 2 {
-            answer += 1
-        }
-    }
-    return answer
+    return (1...n).filter( { i in (1...i).filter( { i % $0 == 0} ).count > 2 } ).count
 }
