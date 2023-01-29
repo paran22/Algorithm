1c4477af00a77f379ff1947874b0a97a1783666f
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-29 18:57:48 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.swift
+++ 프로그래머스/lv0/120891. 369게임/369게임.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ order:Int) -> Int {
-    var answer = 0
-    for i in Array(String(order)) {
-        if ["3", "6", "9"].contains(i) {
-            answer += 1
-        }
-    }
-    return answer
+    return Array(String(order)).filter( { ["3", "6", "9"].contains($0) } ).count
 }
