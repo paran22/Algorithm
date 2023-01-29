ea1f43142735335bb7e75aef4a20500f5dca0e93
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-29 17:16:09 +0900

    [level 0] Title: 약수 구하기, Time: 0.08 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.swift
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    var answer: [Int] = []
-    for i in 1...n {
-        if n % i == 0 {
-            answer.append(i)
-        }
-    }
-    return answer
+    return (1...n).filter( { n % $0 == 0} )
 }
