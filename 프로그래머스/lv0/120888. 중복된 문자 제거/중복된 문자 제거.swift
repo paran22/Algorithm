7825996043766e67bb41dc804a569c4e6b0b3083
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-01-29 19:26:45 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.02 ms, Memory: 15.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    var answer = ""
+    for i in my_string {
+        if answer.contains(i) {
+            continue
+        }
+        answer += String(i)
+    }
+    return answer
+}
