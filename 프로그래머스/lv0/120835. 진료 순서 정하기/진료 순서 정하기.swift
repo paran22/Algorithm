276ebd3aac9b4c4a23a57676b2f135c2faad1342
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-02-12 19:15:07 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.10 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.swift
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ emergency:[Int]) -> [Int] {
-    let sorted = emergency.sorted(by: >)
-    var answer: [Int] = []
-    for i in emergency {
-        answer.append(sorted.firstIndex(of: i)! + 1)
-    }
-    return answer
+    return emergency.map( { emergency.sorted(by: >).firstIndex(of: $0)! + 1 } )
 }
