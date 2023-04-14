a0013a43f9cc6f8c0fe66cfdc863a34c26d8f8ce
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-14 12:04:12 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.22 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.swift
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.swift
@@ -2,17 +2,9 @@ import Foundation
 
 func solution(_ numbers:String) -> Int64 {
     let array = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    var temp = numbers
-    var answer = "0"
-    while !temp.isEmpty {
-        for i in 0..<array.count {
-            let number = array[i]
-            if temp.starts(with: number) {
-                answer += String(i)
-                let index = temp.index(temp.startIndex, offsetBy: number.count)
-                temp.removeSubrange(temp.startIndex..<index)
-            }
-        }
+    var answer = numbers
+    for i in 0..<array.count {
+        answer = answer.replacingOccurrences(of: array[i], with: String(i))
     }
     return Int64(answer)!
 }
