8476b4e041a84feeefe76091385f3733a528d512
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-21 11:00:43 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
@@ -1,27 +1,10 @@
 import Foundation
 
 func solution(_ dots:[[Int]]) -> Int {
-    var targetA: Int?
-    var targetB: Int?
-    var a: Int?
-    var b: Int?
-    var length1 = 0
-    var length2 = 0
-    for i in dots {
-        if targetA == nil {
-            targetA = i[0]
-            a = i[1]
-            continue
-        }
-        if i[0] == targetA! {
-            length1 = a! - i[1]
-        }
-        if targetB == nil {
-            targetB = i[1]
-            b = i[0]
-            continue
-        }
-        length2 = b! - i[0]
-    }
-    return abs(length1 * length2)
+    let groupX = Dictionary(grouping: dots, by: { $0[0] } )
+    let groupY = Dictionary(grouping: dots, by: { $0[1] } )
+    let x = groupX[groupX.keys.first!]!.map { $0[1] }
+    let y = groupY[groupY.keys.first!]!.map { $0[0] }
+    
+    return (x.max()! - x.min()!) * (y.max()! - y.min()!)
 }
