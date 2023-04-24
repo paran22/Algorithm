e44c813e4d04802832f5b9437aa3fc2c7306167c
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-24 11:28:39 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
-    for i in 0..<num_list.count {
-        if num_list[i] < 0 {
-            return i
-        }
-    }
-    return -1
+    return Int(num_list.firstIndex { $0 < 0 } ?? -1 )
 }
