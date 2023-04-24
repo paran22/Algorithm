dc5f7b80a1fdc75a0575ea65adaaf3ca65b802f4
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-24 11:22:25 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.swift
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String) -> [String] {
-    return my_string.components(separatedBy: " ")
+    return my_string.split(separator: " ").map { String($0) }
 }
