4aa9e7f8c75c4d27e1acfca3c9f47a626db1a76c
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-24 11:03:45 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.swift
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ n:Int) -> String {
-    let index = my_string.index(my_string.startIndex, offsetBy: my_string.count - n)
-    return String(my_string[index...])
+    return String(my_string.suffix(n))
 }
