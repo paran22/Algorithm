4b6ea5fcfde17566cf5dde02b08961a2d8da9300
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-24 10:27:48 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.swift
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ k:Int) -> String {
-    var answer = ""
-    for _ in 0..<k {
-        answer += my_string
-    }
-    return answer
+    return String(repeating: my_string, count: k)
 }
