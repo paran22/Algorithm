2aca07c1b2e9e50c2e716c2a899c902bdc8210f0
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-24 10:09:00 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.swift
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> Int {
-    for i in num_list {
-        if i == n {
-            return 1
-        }
-    }
-    return 0
+    return num_list.contains(n) ? 1 : 0
 }
