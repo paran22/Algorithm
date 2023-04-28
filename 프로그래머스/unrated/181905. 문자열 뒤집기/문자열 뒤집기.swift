42f5ab005e1010a760b6cd3aae0d018fe62f1dc2
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-28 16:02:22 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.18 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.swift
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.swift
@@ -1,11 +1,8 @@
 import Foundation
 
 func solution(_ my_string:String, _ s:Int, _ e:Int) -> String {
-    var answer = String(my_string[my_string.startIndex..<my_string.index(my_string.startIndex, offsetBy: s)])
-    for i in stride(from: e, through: s, by: -1) {
-        answer += String(my_string[my_string.index(my_string.startIndex, offsetBy: i)])
-    }
-    
-    answer += String(my_string[my_string.index(my_string.startIndex, offsetBy: e + 1)...])
-    return answer
+    let prefix = my_string.prefix(s)
+    let suffix = my_string.suffix(my_string.count - e - 1)
+    let middle = Array(my_string)[s...e].reversed().map{ String($0) }.joined()
+    return prefix + middle + suffix
 }
