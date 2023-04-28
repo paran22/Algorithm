811f3d340bca61f5e3f91174f776cf3677728f81
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-28 10:11:08 +0900

    [unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.03 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.swift
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ alp:String) -> String {
-    return my_string.map { String($0) == alp ? String($0).uppercased() : String($0) }.joined()
+    return my_string.map { String($0) == alp ? $0.uppercased() : $0.lowercased() }.joined()
 }
