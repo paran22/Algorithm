4fe316f6f2f99bdf70c97a49cfcfed3dc1e9ad74
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-04-28 10:17:49 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.swift
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    var answer : [Int] = []
-    for i in stride(from: 0, through: num_list.count - 1, by: n) {
-        answer.append(num_list[i])
-    }
-    return answer
+    return stride(from: 0, through: num_list.count - 1, by: n).map { num_list[$0] }
 }
