8845efc66ea5a66bdc646b4720c358eb588e94f8
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-07-28 12:48:13 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,9 +1,3 @@
 function solution(cipher, code) {
-  let answer = '';
-  for (i in cipher) {
-    if (i % code === code - 1) {
-      answer += cipher[i];
-    }
-  }
-  return answer;
+    return cipher.split('').filter((_, index) => (index + 1) % code === 0).join('');
 }
