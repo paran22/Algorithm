dd9129a3c72ef225d8a384b05d4583f826fcd8ab
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-07-28 16:39:23 +0900

    [level 0] Title: 합성수 찾기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let answer = 0;
     for (let i = 1; i < n + 1; i++) {
-        if (Array.from(Array(i).keys()).map((e) => e + 1).filter((num) => i % num === 0).length > 2) {
+        if (Array(i).fill().map((_, index) => index + 1).filter((num) => i % num === 0).length > 2) {
             answer += 1;
         }
     }
