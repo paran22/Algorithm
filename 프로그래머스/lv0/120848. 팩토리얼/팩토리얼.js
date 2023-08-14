e1aa7a4957d4874ac90a2f1b54b3267c8150ada7
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-08-14 10:17:48 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,11 +1,9 @@
 function solution(n) {
-    const factorial = (num) => {
-        if (num === 1) return 1;
-        return factorial(num - 1) * num;
-    };
     let answer = 1;
-    while (factorial(answer) <= n) {
+    let factorial = 1;
+    while (factorial <= n) {
         answer += 1;
+        factorial *= answer;
     }
     return answer - 1;
 }
