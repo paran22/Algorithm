a893d0763675d24f183de60c64f57fd7bbba3623
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-10-28 23:31:44 +0900

    [level unrated] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
@@ -1,4 +1,3 @@
 function solution(numlist, n) {
-    let sorted = numlist.map(num => [num, Math.abs(num - n)]).sort((a, b) =>  a[1] - b[1] || b[0] - a[0]);
-    return sorted.map(num => num[0]);
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
