07ac3930828bbc4bd025ab40b98a5cea51fe371f
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2023-11-21 09:55:17 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,8 +1,3 @@
 function solution(s) {
-    const first = s.substring(0, 1);
-    if (first === "-" | first === "+") {
-        const num = parseInt(s.substring(1))
-        return first === "-" ? num * -1 : num;
-    }
-    return parseInt(s);
+    return s/1;
 }
