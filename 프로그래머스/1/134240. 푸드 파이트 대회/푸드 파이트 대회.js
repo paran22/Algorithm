90c81a1f74e21e96065245fe65ed09da3b172f22
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-02-16 12:59:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -2,8 +2,7 @@ function solution(food) {
     let order = '';
     for (i = 1; i < food.length; i++) {
         const half = Math.floor(food[i] / 2)
-        console.log(half)
         order += String(i).repeat(half);
     }
-    return order + "0" + order.split('').reverse().join('');
+    return order + "0" + [...order].reverse().join('');
 }
