ce86302968a2dcd410c0a99ca3bfc48f59bc275b
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-07-28 23:36:57 +0900

    [level 2] Title: 하노이의 탑, Time: 12.32 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = []
+
+    def move(start, to):
+        answer.append([start, to])
+
+    def hanoi(N, start, to, via):
+        if N == 1:
+            move(start, to)
+        else:
+            hanoi(N - 1, start, via, to)
+            move(start, to)
+            hanoi(N - 1, via, to, start)
+
+    hanoi(n, 1, 3, 2)
+    return answer
