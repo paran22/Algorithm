151e02c40a718e95f71f349e1187020b4341f908
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-08-18 23:06:21 +0900

    [level 2] Title: 프로세스, Time: 1.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -4,9 +4,8 @@ def solution(priorities, location):
     queue = deque((index, value) for index, value in enumerate(priorities))
     tries = 0
     while queue:
-        max_priority = max(queue, key=lambda x: x[1])[1]
         order, process = queue.popleft()
-        if max_priority > process:
+        if any(process < q[1] for q in queue):
             queue.append((order, process))
             continue
         tries += 1
