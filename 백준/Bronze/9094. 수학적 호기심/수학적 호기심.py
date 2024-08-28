29e366f3664ff682252bf8ac09832c671d4dd0a0
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-08-29 00:02:32 +0900

    [Bronze III] Title: 수학적 호기심, Time: 3332 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,11 +1,18 @@
-memory = [[0] * 101 for _ in range(101)]
-for b in range(2, 101) :
-    for a in range(1, b) :
-        memory[b][a] = a ** 2 + b ** 2
-for _ in range(int(input())) :
-    n, m = map(int, input().split())
-    cnt = 0
-    for b in range(2, n) :
-        for a in range(1, b) :
-            if (memory[b][a] + m) % (a * b) == 0 : cnt += 1
-    print(cnt)
+import sys
+from collections import defaultdict
+
+T = int(sys.stdin.readline().strip())
+dict = defaultdict(dict)
+
+for b in range(2, 101):
+    for a in range(1, b):
+        dict[b][a] = a**2 + b**2
+
+for _ in range(T):
+    n, m = map(int, sys.stdin.readline().split())
+    count = 0
+    for b in range(2, n):
+        for a in range(1, b):
+            if (dict[b][a] + m) % (a * b) == 0:
+                count += 1
+    print(count)
