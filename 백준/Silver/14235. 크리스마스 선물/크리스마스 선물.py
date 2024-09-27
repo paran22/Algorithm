b57ab3d03ab64fbb5039f0d6321290620b5953d4
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-09-27 22:07:14 +0900

    [Silver III] Title: 크리스마스 선물, Time: 104 ms, Memory: 40920 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -5,14 +5,13 @@ import heapq
 
 n = int(sys.stdin.readline().strip())
 heap = []
-heapq.heapify(heap)
 
 for _ in range(n):
     num = sys.stdin.readline().strip()
     if num == "0":
-        answer = -heapq.heappop(heap) if len(heap) > 0 else -1
+        answer = -heapq.heappop(heap) if heap else -1
         print(answer)
     else:
         gifts = list(map(int, num.split()))[1::]
-        for i in range(len(gifts)):
-            heapq.heappush(heap, -gifts[i])
+        for gift in gifts:
+            heapq.heappush(heap, -gift)
