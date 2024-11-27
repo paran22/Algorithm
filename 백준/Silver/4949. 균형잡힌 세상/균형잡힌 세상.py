cfd31d6371335f1e900366fbd122ee3579c0e80c
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-11-27 20:26:06 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -5,7 +5,7 @@ while (1):
         break
 
     stack_check_list = []
-    check = 1
+    check = True
     for i in vps:
         if i == '(':
             stack_check_list.append(0)
@@ -13,20 +13,20 @@ while (1):
             stack_check_list.append(1)
         elif i == ')':
 
-            if len(stack_check_list) == 0:
-                check = 0
+            if not stack_check_list:
+                check = False
                 break
             if stack_check_list.pop() != 0:  # 엇갈렸을때
-                check = 0
+                check = False
                 break
         elif i == ']':
-            if len(stack_check_list) == 0:
-                check = 0
+            if not stack_check_list:
+                check = False
                 break
             if stack_check_list.pop() != 1:  # 엇갈렸을때
-                check = 0
+                check = False
                 break
-    if len(stack_check_list) == 0 and check:
+    if not stack_check_list and check:
         print("yes")
     else:
         print("no")
