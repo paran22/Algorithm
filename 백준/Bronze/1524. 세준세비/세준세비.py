cee65990f7b6314e69b7b04433eda90afeaa16f0
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2024-11-27 22:43:26 +0900

    [Bronze I] Title: 세준세비, Time: 120 ms, Memory: 35816 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.py
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -1,25 +1,15 @@
-# https://www.acmicpc.net/problem/1524
+T = int(input())
 
-import sys
-
-T = int(sys.stdin.readline().strip())
-
-for i in range(T):
-    sys.stdin.readline()
-    S, B = map(int, sys.stdin.readline().split())
-    S_soldiers = sorted(list(map(int, sys.stdin.readline().split())))
-    B_soldiers = sorted(list(map(int, sys.stdin.readline().split())))
+for _ in range(T):
+    input()  # 빈 줄 처리
+    N, M = map(int, input().split())
     
-    s_index, b_index = 0, 0  # 인덱스를 사용하여 리스트 순회
-    while s_index < len(S_soldiers) and b_index < len(B_soldiers):
-        if S_soldiers[s_index] < B_soldiers[b_index]:
-            s_index += 1
-        else:
-            b_index += 1
+    # 각 군대의 최강 병사만 찾기
+    sj_max = max(map(int, input().split()))
+    sb_max = max(map(int, input().split()))
     
-    if s_index < len(S_soldiers):
+    # 최강 병사 비교
+    if sj_max >= sb_max:
         print("S")
-    elif b_index < len(B_soldiers):
-        print("B")
     else:
-        print("C")
+        print("B")
