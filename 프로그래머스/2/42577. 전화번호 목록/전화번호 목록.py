56747eab8162bf90f8dffb1ef6ae7ecf8043a136
Kim Gaeun <97428216+paran22@users.noreply.github.com>
2025-01-17 12:58:21 +0900

    [level 2] Title: 전화번호 목록, Time: 535.81 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,21 @@
 def solution(phone_book):
-    phone_dict = {}
-    for num in phone_book:
-        phone_dict[num] = True
-        
-    for num in phone_book:
-        temp = ""
-        for digit in num:
-            temp += digit
-            if temp in phone_dict and temp != num:
+#     sorted_phone_book = sorted(phone_book, key=len)
+#     length = len(phone_book)
+    
+#     for i in range(0, length):
+#         for j in range(i + 1, length):
+#             prefix, number = sorted_phone_book[i], sorted_phone_book[j]
+#             if len(prefix) > len(number):
+#                 break
+#             if number.startswith(prefix):
+#                 return False
+#     return True
+    phone_book_dict = {value: True for value in phone_book}
+
+    for number in phone_book:
+        for i in range(1, len(number)):
+            prefix = number[0:i]
+            if prefix in phone_book_dict:
                 return False
             
     return True
